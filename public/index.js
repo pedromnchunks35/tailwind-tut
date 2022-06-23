3f167d9842db93c4adf38c7bdafdb3c07be49620
pedrochunks35 <a26899@sa-miranda.net>
2022-06-23 10:55:54 +0100

    tailwind tut

---
+++ public/index.js
@@ -0,0 +1,11 @@
+const burguer = document.querySelector('#burger');
+
+const menu = document.querySelector("#menu");
+
+burguer.addEventListener('click',()=>{
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }else{
+        menu.classList.add('hidden');
+    }
+})
